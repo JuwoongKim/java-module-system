4963dfdaaf74c6a6d94715425b742702bc9b9bf3
juwoong kim <wwong7247@gmail.com>
2023-08-19 17:37:35 +0900

    ✨ base 모듈의 로깅 의존성을 api로 적용해 base 의존 모듈에서 로깅 사용을 테스트하다

---
+++ base/src/main/java/base/Chicken.java
@@ -0,0 +1,15 @@
+package base;
+
+public class Chicken {
+
+	private String name;
+
+	public Chicken(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
--- client/src/main/java/client/StudentClient.java
+++ client/src/main/java/client/StudentClient.java
@@ -1,15 +1,37 @@
 package client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import base.Chicken;
 import dbimpl.StudentDbService;
 import model.Student;
 import service.StudentService;
 
 public class StudentClient {
+	private static final Logger logger = LogManager.getLogger(StudentClient.class);
+
+	public StudentClient() {
+		logger.info("StudentClient start");
+
+	}
+
 	public static void main(String[] args) {
+		StudentClient studentClient = new StudentClient();
+		studentClient.chickenTest();
+
+	}
+
+	public void studentTest(){
 		StudentService service = new StudentDbService();
 		service.create(new Student());
 		service.read("17SS0001");
 		service.update(new Student());
 		service.delete("17SS0001");
 	}
+
+	public void chickenTest(){
+		logger.info("chickenTest start");
+		Chicken chicken =new Chicken("Very delicious");
+	}
+
 }
